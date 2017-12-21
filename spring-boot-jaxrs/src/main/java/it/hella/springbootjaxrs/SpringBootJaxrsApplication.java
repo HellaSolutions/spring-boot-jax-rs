6436f0f654edebad79fc0fc3ac5dd6e58032565e
Paola <paola.greco@hella-solutions.com>
2017-12-21 15:40:32 +0100

    First commit

---
+++ spring-boot-jaxrs/src/main/java/it/hella/springbootjaxrs/SpringBootJaxrsApplication.java
@@ -0,0 +1,12 @@
+package it.hella.springbootjaxrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJaxrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJaxrsApplication.class, args);
+	}
+}
